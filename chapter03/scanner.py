a2c3e8e95b2e6815bd3bd6e06e67065f5d7daf89
colind0pe <1370227996@qq.com>
2022-07-06 23:32:16 +0800

    提交第三章代码

---
+++ chapter03/scanner.py
@@ -0,0 +1,146 @@
+import os
+import socket
+import struct
+import threading
+
+from ctypes import *
+from ipaddress import ip_address, ip_network
+
+# 监听主机
+host = "192.168.124.2"
+# 目标子网
+tgt_subnet = "192.168.124.0/24"
+# 自定义字符串，将在ICMP响应中进行核对
+tgt_message = "PYTHONRULES!"
+
+
+# 批量发送UDP数据包
+def udp_sender(sub_net, magic_message):
+    # 创建socket对象(SOCK_DGRAM:UDP客户端)
+    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # 尝试发送magic_message这个消息到子网的每个ip
+    for ip in ip_network(sub_net).hosts():
+        sender.sendto(magic_message.encode('utf-8'), (str(ip), 65212))
+
+
+# ip头定义
+class IP(Structure):
+    _fields_ = [
+        ("ihl",                 c_ubyte, 4),    # ip head length:头长度
+        ("version",             c_ubyte, 4),    # 版本
+        ("tos",                 c_ubyte),       # 服务类型
+        ("len",                 c_ushort),      # ip数据包总长度
+        ("id",                  c_ushort),      # 标识符
+        ("offset",              c_ushort),      # 片偏移
+        ("ttl",                 c_ubyte),       # 生存时间
+        ("protocol_num",        c_ubyte),       # 协议数字，应该是协议类型,这里用数字来代表时哪个协议,下面构造函数有设置映射表
+        ("sun",                 c_ushort),      # 头部校验和
+        ("src",                 c_ulong),       # 源ip地址
+        ("dst",                 c_ulong)        # 目的ip地址
+    ]
+
+
+    # 创建对象时调用，返回当前对象的一个实例;注意：这里的第一个参数是cls即class本身
+    def __new__(cls, socket_buffer=None):
+        return cls.from_buffer_copy(socket_buffer)
+
+    # 创建完对象后调用，对当前对象的实例的一些初始化，无返回值,即在调用__new__之后，根据返回的实例初始化；注意，这里的第一个参数是self即对象本身【注意和new的区别】
+    def __init__(self, socket_buffer=None):
+        # 协议字段与协议名称的对应
+        self.protocol_map = {1:"ICMP", 6:"TCP", 17:"UDP"}
+        
+        # 可读性更强的ip地址(转换32位打包的IPV4地址为IP地址的标准点号分隔字符串表示。)
+        self.src_address = socket.inet_ntoa(struct.pack("@I", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("@I", self.dst))
+
+        # 协议类型
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
+
+
+class ICMP(Structure):
+    _fields_ = [
+        ("type",            c_ubyte),   # 类型
+        ("code",            c_ubyte),   # 代码值
+        ("checksum",        c_ushort),  # 头部校验和
+        ("unused",          c_ushort),  # 未使用
+        ("next_hop_mtu",    c_ushort)   # 下一跳的MTU
+    ]
+
+    def __new__(cls, socket_buffer):
+        return cls.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer):
+        self.socket_buffer = socket_buffer
+
+
+if os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+# 这里端口为0,监听所有端口
+sniffer.bind((host, 0))
+
+# 设置在捕获的数据包中包含IP头
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+# 在Windows平台上,我们需要设置IOCTL以启用混杂模式
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+# 开启多线程发送udp数据包
+t = threading.Thread(target=udp_sender, args=(tgt_subnet, tgt_message))
+t.start()
+
+try:
+    while True:
+        # 读取数据包
+        raw_buffer = sniffer.recvfrom(65565)[0]
+
+        # 将缓冲区的前20个字节按IP头进行解析
+        ip_header = IP(raw_buffer[0:20])
+
+        # 输出协议和通信双方IP地址
+        print("Protocol: %s %s -> %s" % (
+            ip_header.protocol,
+            ip_header.src_address,
+            ip_header.dst_address
+        ))
+
+        # 如果为ICMP,进行处理
+        if ip_header.protocol == "ICMP":
+
+             # 计算ICMP包的起始位置,并获取ICMP包的数据
+             # ihl是头部长度,代表32位(即4字节)长的分片的个数
+            offset = ip_header.ihl * 4
+            buf = raw_buffer[offset:offset + sizeof(ICMP)]
+
+            # 解析ICMP数据
+            icmp_header = ICMP(buf)
+
+            print("ICMP -> Type: %d Code: %d" % (
+                icmp_header.type,
+                icmp_header.code
+        ))
+
+            # 检查类型和代码值是否都为3
+            if icmp_header.code == 3 and icmp_header.type == 3:
+                # 确认响应的主机再我们的目标子网之内
+                if ip_address(ip_header.src_address) in ip_network(tgt_subnet):
+                    # 确认ICMP包中包含我们发送的自定义的字符串
+                    if raw_buffer[len(raw_buffer) 
+                        - len(tgt_message):] == tgt_message:
+                        print("Host Up: %s" % ip_header.src_address)
+
+
+# 处理CTRL-C
+except KeyboardInterrupt:
+    # 如果运行在Windows上,关闭混杂模式
+    if os.name == "nt":
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
---
+++ chapter03/sniffer.py
@@ -0,0 +1,29 @@
+import socket
+import os
+
+# 监听主机，即监听那个网络接口，下面的为我的kali的ip
+host = "192.168.124.2"
+
+# 创建原始套接字,然后绑定在公开接口上
+if  os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+# 这里端口为0,监听所有端口
+sniffer.bind((host, 0))
+
+# 设置在捕获的数据包中包含IP头
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+# 在Windows平台上,我们需要设置IOCTL以启用混杂模式
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+# 读取单个数据包
+print(sniffer.recvfrom(65565))
+
+# 在Windows平台上关闭混杂模式
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
